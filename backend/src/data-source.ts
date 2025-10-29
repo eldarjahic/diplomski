0bc2ed85412ba2628f99a45849e2f9d067809e44
eldarjahic <eldar.jahic@stu.ibu.edu.ba>
2025-10-29 20:28:56 +0100

    initial project setup backend

---
+++ backend/src/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASS || "password",
+  database: process.env.DB_NAME || "diplomski",
+  synchronize: true,  // ⚠️ Only for dev, auto-sync schema
+  logging: true,
+  entities: [__dirname + "/entity/*.ts"],
+  migrations: [__dirname + "/migration/*.ts"],
+  subscribers: [],
+});
---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import "reflect-metadata"
+import { AppDataSource } from "./data-source";
+import { User } from './entity/user';
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Data Source has been initialized!");
+
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Error during Data Source initialization:", err);
+  });
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+app.get('/users/:userId', async (req, res) => {
+  const userId  = req.params.userId;
+  const userRepository = AppDataSource.getRepository(User)
+  const user = await userRepository.findOneBy({
+      id: Number(userId),
+  })
+
+  res.send({user: user})
+})
