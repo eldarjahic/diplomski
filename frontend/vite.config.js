2d3a10236e7b55940ea2a9e4957097df0963fc1f
eldarjahic <eldar.jahic@stu.ibu.edu.ba>
2025-12-16 21:59:43 +0100

    vite config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: [
+      "eldi.deployer3000.halvooo.com",
+      ".deployer3000.halvooo.com",
+    ],
+  },
 });
